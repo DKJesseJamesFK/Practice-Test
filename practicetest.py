f1ad963cada3e4e6c845cb1d3c2e3e52f4ee1a30
DKJesseJamesFK <jfry59@ivytech.edu>
2024-09-30 02:26:56 -0400

    Added more notes and separate panels for T/F and multiple choice questions.

--- practicetest.py
+++ practicetest.py
@@ -9,25 +9,68 @@ class PracticeTest(EasyFrame):
         self.addButton(text = "Take Test", row = 0, column = 1)
 
     def newTest(self):
+        """
+        Opens a new dialog for creating a new practice test.
+        """
         class NewTest(EasyFrame):
             def __init__(self):
                 EasyFrame.__init__(self, title = "New Test")
+                # Set starting question number
                 number = 1
+                # Create widgets
                 self.addLabel(text = "Enter Title of Test", row = 0, column = 0)
                 self.newTitle = self.addTextField(text = "", row = 0, column = 1)
                 self.enterBtn = self.addButton(text = "Enter", row = 0, column = 2, command = self.enterTitle)
                 self.addLabel(text = f"Question number {number}", row = 1, column = 0)
                 self.questionFormat = self.addRadiobuttonGroup(row = 1, column = 1, orient = "horizontal")
-                self.multipleChoice = self.questionFormat.addRadiobutton(text = "Multiple Choice")
-                self.trueFalse = self.questionFormat.addRadiobutton(text = "True/False")
-                self.questionFormat.setSelectedButton(self.multipleChoice)
+                self.multipleChoice = self.questionFormat.addRadiobutton(text = "Multiple Choice", command = self.setMultipleChoice)
+                self.trueFalse = self.questionFormat.addRadiobutton(text = "True/False", command = self.setTrueFalse)
+
+                # Add panels for true/false
+                self.trueFalseFrame = self.addPanel(row = 2, column = 0, columnspan = 3)
+                self.trueFalseFrame.addLabel(text = "Question:", row = 0, column = 0)
+                self.trueFalseFrame.addTextArea(text = "", row = 1, column = 0, width = 30, height = 5, columnspan = 3)
+                self.trueFalseFrame.addLabel(text = "Answer:", row = 2, column = 0)
+                self.trueFalseAnswer = self.trueFalseFrame.addRadiobuttonGroup(row = 3, column = 0, orient = "horizontal")
+                self.true = self.trueFalseAnswer.addRadiobutton(text = "True")
+                self.false = self.trueFalseAnswer.addRadiobutton(text = "False")
+
+                # Add panels for multiple choice
+                self.multipleChoiceFrame = self.addPanel(row = 2, column = 0, columnspan = 3)
+                self.multipleChoiceFrame.addLabel(text = "Question:", row = 0, column = 0)
+                self.multipleChoiceFrame.addTextArea(text = "", row = 1, column = 0, width = 30, height = 5, columnspan = 3)
+                self.multipleChoiceFrame.addLabel(text = "Answer:", row = 2, column = 0)
                 
-            
+                
             def enterTitle(self):
+                """
+                Called when the user presses the "Enter" button. Opens a
+                .txt file with the name of the test, and disables the button
+                and the text field. Also selects the "Multiple Choice" radio
+                button.
+                """
                 open(self.newTitle.getText() + ".txt", "w")
                 self.enterBtn.configure(state = "disabled")
                 self.newTitle.configure(state = "readonly")
-                
+                self.questionFormat.setSelectedButton(self.multipleChoice)
+
+            def setTrueFalse(self):
+                """
+                Called when the user selects the "True/False" radio button. Lifts
+                the panel for entering true/false questions, and lowers the panel
+                for entering multiple choice questions.
+                """
+                self.trueFalseFrame.lift()
+                self.multipleChoiceFrame.lower()
+
+            def setMultipleChoice(self):
+                """
+                Called when the user selects the "Multiple Choice" radio button. Lifts
+                the panel for entering multiple choice questions, and lowers the panel
+                for entering true/false questions.
+                """
+                self.multipleChoiceFrame.lift()
+                self.trueFalseFrame.lower()
                 
         NewTest().mainloop()
 
